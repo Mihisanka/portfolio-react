9f79408e04181d2cf818085a9e686d4785d49275
Mihisanka Sandudeeptha <mihisanka1998@gmail.com>
2024-02-06 17:42:16 +0530

    home page
    adding animation to home page text and fix some bugs

---
+++ portfolio/src/components/Home/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import LogoTitle from '../../assets/images/logo-s.png'
+import './index.scss';
+import { Link } from 'react-router-dom';
+
+ const Home=() => {
+  return (
+    <div className="container home-page">
+        <div className="text-zone">
+        <h1>Hi,
+        <br/>I'm
+            <img src={LogoTitle} alt="developer"/>
+            andudeeptha
+            
+            developer
+            </h1>
+            <h2>A passionate frontend web developer with <br/><br/> React / javaScript <br/><br/>  Background in SofweraEnginering.</h2>
+            <Link to="/contact" className="flat-button">Contact Me</Link>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Home;
